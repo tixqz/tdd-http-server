973ee350f54e7e9274c64e7babf07ac70b9db922
tixqz <chief9441@gmail.com>
2019-10-18 23:58:03 +0300

    Added StubPlayerStore struct in test

--- server_test.go
+++ server_test.go
@@ -7,8 +7,23 @@ import (
 	"testing"
 )
 
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
 func TestGETPlayers(t *testing.T) {
-	server := &PlayerServer{}
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
 
 	t.Run("return Pepper's score", func(t *testing.T) {
 		request := newGetPlayerScore("Pepper")
